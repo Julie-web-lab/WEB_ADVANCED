802efc896fcf827057b7f6724d94bb9f6d99bcd2
julie-web-lab <julietkabasomi20@gmail.com>
2022-04-17 10:00:32 +0300

    Database, creation, tables,migration
    Database, creation, tables, migration

--- LARAVEL/DiTrendz/resources/views/Layouts/main_layout.blade.php
+++ LARAVEL/DiTrendz/resources/views/Layouts/main_layout.blade.php
@@ -5,20 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DiTrendz-.....@yield('title')</title>
+    <link rel="stylesheet" href="bootstrap.css">
 </head>
-<body>    
-    <hr>
-<ul>
+<body class="container">    
+  <div class="row mt-5">
+    <div class="col-md-4">
+        <ul class="list-group">
     
-<li> <a href="/">Home Page </a>  </li>
-<li> <a href="/About">About Us </a>  </li>
-<li> <a href="/Contact">Contact Us </a>  </li>
-<hr>
-</ul>
-
+            <a class="list-group-item" href="/">Home Page </a>  
+            <a class="list-group-item" href="/About">About Us </a>  
+            <a class="list-group-item" href="/Contact">Contact Us </a> 
+            <hr>
+            </ul>
+             
+    </div>
+    <div class="col-md-8 border left">
+          
 <h2>@yield('heading')</h2>
 
-@yield('Content')
+@yield('Content')  
+    </div>  
+</div> 
+
+
 
 <hr>
 
